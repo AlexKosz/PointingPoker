b0181a8d4a4141237693cbe8709afff9fea6d599
JillianKoszuta <jilliankoszuta@gmail.com>
2024-11-27 00:48:07 -0500

    Basic form setup and navigation on redux update

--- client/src/main/views/login/Login.jsx
+++ client/src/main/views/login/Login.jsx
@@ -1,58 +1,78 @@
-import "./App.css";
-import io from "socket.io-client";
-import { useEffect, useState } from "react";
-import {
-	Route,
-	Routes,
-	unstable_HistoryRouter as HistoryRouter,
-} from "react-router-dom";
+import './App.css';
+import io from 'socket.io-client';
+import { v4 as uuidv4 } from 'uuid';
 
-const socket = io.connect("http://localhost:3001");
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setUser } from '../../../actions/userActions';
+import { useNavigate } from 'react-router-dom';
+import paths from '../../utils/consts/paths';
+
+const socket = io.connect('http://localhost:3001');
 
 function Login() {
 	//Room State
-	const [room, setRoom] = useState("");
+	const [room, setRoom] = useState('');
+	const [name, setName] = useState('');
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
 
-	// Messages States
-	const [message, setMessage] = useState("");
-	const [messageReceived, setMessageReceived] = useState("");
+	const user = useSelector((store) => store?.user?.user);
 
 	const joinRoom = () => {
-		if (room !== "") {
-			socket.emit("join_room", room);
+		if (name === '' && room === '') {
+			alert('You need to enter your name and a room ID');
+			return;
+		} else if (name === '') {
+			alert('You need to enter your name');
+			return;
+		} else if (room === '') {
+			alert('You need to enter a room ID');
+			return;
 		}
-	};
 
-	const sendMessage = () => {
-		console.log(message, room);
-		socket.emit("send_message", { message, room });
+		console.log('Data was valid', 'Room:', room, 'Name:', name);
+
+		const newUser = { name: name };
+
+		dispatch(setUser(newUser));
 	};
 
 	useEffect(() => {
-		socket.on("receive_message", (data) => {
-			console.log(data);
-			setMessageReceived(data.message);
-		});
-	}, [socket]);
+		console.log('User changed in redux', user, '<- new val');
+		if (user?.name !== '' && room !== '') {
+			console.log('try to navigate to new room', room, '<- room attempted');
+			navigate(`${paths.room(room)}`);
+		}
+	}, [user]);
+
+	// useEffect(() => {
+	// 	socket.on('userList', (data) => {
+	// 		console.log('server sent back', data);
+	// 	});
+	// }, [socket]);
 
 	return (
-		<div className="App">
+		<div className="login">
 			<input
-				placeholder="Room Number..."
+				placeholder="Your name"
+				onChange={(event) => {
+					setName(event.target.value);
+				}}
+			/>
+
+			<hr />
+
+			<input
+				placeholder="Target room"
 				onChange={(event) => {
 					setRoom(event.target.value);
 				}}
 			/>
+
+			<hr />
+
 			<button onClick={joinRoom}> Join Room</button>
-			<input
-				placeholder="Message..."
-				onChange={(event) => {
-					setMessage(event.target.value);
-				}}
-			/>
-			<button onClick={sendMessage}> Send Message</button>
-			<h1> Message:</h1>
-			{messageReceived}
 		</div>
 	);
 }
