567c3e562a806278f6c10d7a8c317a047fc9639d
JillianKoszuta <jilliankoszuta@gmail.com>
2024-11-27 00:49:14 -0500

    Re-create join room socket function

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const http = require("http");
-const { Server } = require("socket.io");
-const cors = require("cors");
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+const { v4: uuidv4 } = require('uuid');
 
 app.use(cors());
 
@@ -11,46 +12,44 @@ const usersInRoom = {}; // Keep track of users in each room
 
 const io = new Server(server, {
 	cors: {
-		origin: "http://localhost:3000",
-		methods: ["GET", "POST"],
+		origin: 'http://localhost:3000',
+		methods: ['GET', 'POST'],
 	},
 });
 
-io.on("connection", (socket) => {
-	console.log(`User Connected: ${socket.id}`);
+io.on('connection', (socket) => {
+	socket.on('joinRoom', (room, user) => {
+		socket.join(room);
 
-	socket.on("joinRoom", (data) => {
-		console.log("D", data);
-		const room = data?.room;
-		socket.join(data);
+		const newUser = { name: user?.name, id: socket.id };
 
 		if (!usersInRoom[room]) {
-			usersInRoom[room] = [];
+			usersInRoom[room] = [newUser];
+		} else if (!usersInRoom[room].some((user) => user.id === socket.id)) {
+			usersInRoom[room].push(newUser);
 		}
 
-		usersInRoom[room].push(socket.id);
-		console.log(usersInRoom[room]);
-		io.emit("userList", usersInRoom[room]);
-		io.to(room).emit("userJoined", socket.id);
-
-		socket.on("disconnect", () => {
-			// Remove user from the room list
-			usersInRoom[room] = usersInRoom[room].filter((id) => id !== socket.id);
-			if (usersInRoom[room].length === 0) {
-				delete usersInRoom[room];
-			} else {
-				// Notify other users in the room about the user leaving
-				socket.to(room).emit("userLeft", socket.id);
-			}
-		});
+		io.emit('userList', usersInRoom[room]);
+		io.to(room).emit('userJoined', newUser);
 	});
 
-	socket.on("sendMessage", (data) => {
+	// socket.on('disconnect', () => {
+	// 	// Remove user from the room list
+	// 	usersInRoom[room] = usersInRoom[room].filter((id) => id !== socket.id);
+	// 	if (usersInRoom[room].length === 0) {
+	// 		delete usersInRoom[room];
+	// 	} else {
+	// 		// Notify other users in the room about the user leaving
+	// 		socket.to(room).emit('userLeft', socket.id);
+	// 	}
+	// });
+
+	socket.on('sendMessage', (data) => {
 		console.log(data);
-		socket.to(data.room).emit("recMessage", data);
+		socket.to(data.room).emit('recMessage', data);
 	});
 });
 
 server.listen(3001, () => {
-	console.log("SERVER IS RUNNING");
+	console.log('SERVER IS RUNNING');
 });
