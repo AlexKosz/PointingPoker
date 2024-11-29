a2ff7e8e8ba69e1d8ae3a4f2b42d66f2180cee77
JillianKoszuta <jilliankoszuta@gmail.com>
2024-11-29 01:14:50 -0500

    made abstain button, set value to null

--- client/src/main/views/pointingRoom/components/mainContent/VotingArea.jsx
+++ client/src/main/views/pointingRoom/components/mainContent/VotingArea.jsx
@@ -3,10 +3,14 @@ import PointCard from './PointCard';
 
 const VotingArea = ({ selectValue, selectedValue }) => {
 	const voteNumbers = [0, 0.5, 1, 2, 3, 4, 5, 6, 7, 8];
+	const isValueNull = selectedValue === null;
+	const abstainBtnClass = isValueNull
+		? 'abstain-button voted'
+		: 'abstain-button';
 
 	return (
-		<section className='voting-area'>
-			<div className='point-cards'>
+		<section className="voting-area">
+			<div className="point-cards">
 				{voteNumbers.map((value, index) => (
 					<PointCard
 						value={value}
@@ -16,7 +20,12 @@ const VotingArea = ({ selectValue, selectedValue }) => {
 					/>
 				))}
 			</div>
-			<button className='abstain-button'>Abstain</button>
+			<button
+				className={abstainBtnClass}
+				onClick={() => selectValue(null)}
+			>
+				Abstain
+			</button>
 		</section>
 	);
 };
