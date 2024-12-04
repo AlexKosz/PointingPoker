9a700b19a0135d775a4b4ee227c6d2985337b786
JillianKoszuta <jilliankoszuta@gmail.com>
2024-12-03 23:57:26 -0500

    get room from param

--- client/src/main/components/Header.jsx
+++ client/src/main/components/Header.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const Header = () => {
-  return (
-   		<header className="header">
-			<h1>Point Poker</h1>
-			<h4>Room: ABSFDSF</h4>
+	const { room } = useParams();
+
+	return (
+		<header className="header">
+			<h1>Pointing Poker</h1>
+			<h4>Room: {room} </h4>
 		</header>
+	);
+};
 
-  )
-}
-
-export default Header
+export default Header;
