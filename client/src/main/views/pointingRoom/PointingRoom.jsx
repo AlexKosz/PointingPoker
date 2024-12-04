cc42ab9961ec3b4c977e966f517f78df879bce0c
JillianKoszuta <jilliankoszuta@gmail.com>
2024-12-03 23:59:25 -0500

    clear vote functionality, join room fix

--- client/src/main/views/pointingRoom/PointingRoom.jsx
+++ client/src/main/views/pointingRoom/PointingRoom.jsx
@@ -8,25 +8,23 @@ import Modal from './components/modal/Modal';
 import { useDispatch, useSelector } from 'react-redux';
 import { setUser, clearUser } from '../../../actions/userActions';
 import paths from '../../utils/consts/paths';
+import Header from '../../components/Header';
 
 const socket = io('http://localhost:3001'); // Replace with your server URL
 
 const PointingRoom = () => {
 	const [searchParams, setSearchParams] = useSearchParams();
-	console.log(searchParams);
-	console.log(useParams());
 	const { room } = useParams();
 	const dispatch = useDispatch();
 	const navigate = useNavigate();
 
 	const [users, setUsers] = useState([]);
-	const [userId, setUserId] = useState(null);
+
 	const [isModalOpen, setIsModalOpen] = useState(false);
 
 	const [selectedValue, setSelectedValue] = useState(null);
 
 	const user = useSelector((state) => state.user.user);
-	console.log('USER:', user);
 
 	useEffect(() => {
 		if (!user) {
@@ -34,39 +32,56 @@ const PointingRoom = () => {
 		}
 
 		if (user) {
-			console.log(room);
 			socket.emit('joinRoom', room, user);
 		}
-	}, [user]);
+	}, []);
+
+	useEffect(() => {
+		socket.on('iJoin', (data) => {
+			console.log('ijoin server sent back', data);
+			dispatch(setUser(data));
+		});
+	}, [socket]);
 
 	useEffect(() => {
 		socket.on('userList', (data) => {
 			console.log('server sent back', data);
 			setUsers(data);
+			const userIndex = data.findIndex((element) => element.id === socket.id);
+			setSelectedValue(data[userIndex].vote);
+			console.log(userIndex);
 		});
 	}, [socket]);
 
 	useEffect(() => {
-		console.log(selectedValue);
-		socket.emit('vote', room, user, selectedValue);
+		if (selectedValue || selectedValue !== null) {
+			console.log('emiting vote', selectedValue);
+			socket.emit('vote', room, user, selectedValue);
+		}
 	}, [selectedValue]);
 
+	const clearVotes = () => {
+		socket.emit('clearVotes', room);
+		setSelectedValue(undefined);
+	};
+
 	return (
-		<div
-			style={{
-				marginTop: '20vh',
-			}}
-		>
-			<UserProfile user={user} />
-			<VoteControls openModal={() => setIsModalOpen(true)} />
-			<MainContent
-				selectedValue={selectedValue}
-				selectValue={(v) => setSelectedValue(v)}
-				participants={users}
-			/>
-			{isModalOpen && <Modal closeModal={() => setIsModalOpen(false)} />}
-			<h1>Room: {room || 'No room founda '}</h1>
-		</div>
+		<>
+			<Header />
+			<div className="mainRoomDiv">
+				<UserProfile user={user} />
+				<VoteControls
+					openModal={() => setIsModalOpen(true)}
+					clearVotes={clearVotes}
+				/>
+				<MainContent
+					selectedValue={selectedValue}
+					selectValue={(v) => setSelectedValue(v)}
+					participants={users}
+				/>
+				{isModalOpen && <Modal closeModal={() => setIsModalOpen(false)} />}
+			</div>
+		</>
 	);
 };
 
