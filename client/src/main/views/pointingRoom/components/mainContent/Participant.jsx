9676d992f569a9e8c80e1f5af2ec14f9c6138435
JillianKoszuta <jilliankoszuta@gmail.com>
2024-12-03 23:58:52 -0500

    display vote and if voted

--- client/src/main/views/pointingRoom/components/mainContent/Participant.jsx
+++ client/src/main/views/pointingRoom/components/mainContent/Participant.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { getInitials } from '../../../../utils/helperFuncs';
 
-const Participant = ({ name, hasVoted }) => {
+const Participant = ({ name, hasVoted, vote }) => {
 	const hasVotedClass = hasVoted ? 'avatar voted' : 'avatar notVoted';
 	return (
-		<div className='participant'>
+		<div className="participant">
 			<div className={hasVotedClass}>{getInitials(name) || ''}</div>
-			<span className='participant-name'>{name}</span>
-			<span className='vote-status'>
+			<span className="participant-name">{name}</span>
+			<span className="vote-status">
 				{hasVoted ? 'Voted' : 'Not yet voted'}
 			</span>
+			<h4>{vote}</h4>
 		</div>
 	);
 };
